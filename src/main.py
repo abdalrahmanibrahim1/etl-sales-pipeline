b4ebfe789414313b15ccca5b39f2e5631aa039db
abdal <aibrahim@adstratech.com>
2026-06-21 17:47:41 +0300

    Included full reporting functions in main

--- src/main.py
+++ src/main.py
@@ -4,7 +4,8 @@ from report import (
     top_product_by_revenue,
     sales_by_city,
     monthly_sales_trend,
-    top_customer_by_spending
+    top_customer_by_spending,
+    revenue_by_category
 )
 
 def main():
@@ -14,6 +15,7 @@ def main():
     top_product_by_revenue()
     sales_by_city()
     monthly_sales_trend()
+    revenue_by_category()
     top_customer_by_spending()
 
 if __name__ == "__main__":
